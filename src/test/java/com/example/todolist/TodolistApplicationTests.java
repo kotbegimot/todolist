b27c55aadbd73cb954d9955f70f82f39639acb17
github <arthur.misyunas@gmail.com>
2023-10-16 21:00:35 +0200

    Minor changes

--- src/test/java/com/example/todolist/TodolistApplicationTests.java
+++ src/test/java/com/example/todolist/TodolistApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.todolist;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles(value = "dev")
 class TodolistApplicationTests {
   @Test
   void runTest() {
